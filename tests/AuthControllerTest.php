795d67b9dda1d7ddbffba8b9c49a35c9220c7aab
attenberger <attenberger@ta.de>
2021-09-03 08:42:00 +0200

    added dummy tests

---
+++ tests/AuthControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    public function testSomething(){
+        $this->assertTrue(true,true);
+    }
+}
---
+++ tests/PostControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    public function testSomething(){
+        $this->assertTrue(true,true);
+    }
+}
